c3e029b4e56300c0f7213d3f7c06cf632beb3531
Ever Blua <everblua@gmail.com>
2023-10-24 01:34:29 -0300

    tests config working

--- jest.config.js
+++ jest.config.js
@@ -1,13 +1,13 @@
 module.exports = {
   collectCoverage: true,
-  clearMocks: true,
-  collectCoverageFrom: ['src/**/*.{js,jsx}'],
+  // clearMocks: true,
+  collectCoverageFrom: ['src/**/*.test.{js,jsx}'],
   coverageDirectory: 'coverage',
   // setupFiles: ['./setupTests.js'],
-  setupFilesAfterEnv: ['<rootDir>/setupTests.js'],
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
   testEnvironment: 'jsdom',
-  moduleFileExtensions: ['js', 'jsx'],
-  transform: {
-    '^.+\\.js$': 'babel-jest',
-  },
+  // moduleFileExtensions: ['js', 'jsx'],
+  // transform: {
+  //   '^.+\\.js$': 'babel-jest',
+  // },
 };
--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,4 +1,5 @@
 import { render } from '@testing-library/react';
+import React from 'react';
 import MyApp from './MyApp';
 
 test('render app', () => {
